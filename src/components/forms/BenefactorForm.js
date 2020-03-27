ad8182f736c15b38f47167d509b87f3702dbed26
Justin Trombley <jtrombley96@gmail.com>
2020-03-27 15:22:57 -0400

    Benefactor/Sponsor Form 2.0 Taking in Data
    Co-authored-by: ebisLab <ebaiden@techie.com>

--- src/components/forms/BenefactorForm.js
+++ src/components/forms/BenefactorForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 
 import { Form, Input, Button, Row, Col, Select } from 'antd';
 
@@ -8,44 +8,70 @@ const { TextArea } = Input;
 const { Search } = Input;
 
 const BenefactorForm = () => {
-	const { register, handleSubmit, errors } = useForm();
+	const { control, handleSubmit, errors } = useForm();
 	const onSubmit = data => {
 		console.log(data);
 	};
 
+	const fieldProps = {
+		colon : false,
+	};
+
 	return (
-		<Form onSubmit={handleSubmit(onSubmit)}>
-			<Form.Item name=''>
-				<Select style={{width: 400}}>
-					<Option>Benefactor / Sponsor Form</Option>
-				</Select>
-			</Form.Item>
-			<Form.Item name=''>
-				<Search placeholder='Search for record' onSearch={value => console.log(value)} style={{ width: 400 }} />
-			</Form.Item>
-			<h1>Benefactor / Sponsor Form</h1>
-			<Row>
-				<Col>
-					<Form.Item name='' label='Listing notes'>
-						<TextArea placeholder='Listing notes' style={{ width: 600 }} />
-					</Form.Item>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<Form.Item name='' label='Personal notes'>
-						<TextArea placeholder='Personal notes' style={{ width: 600 }} />
-					</Form.Item>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<Form.Item name='' label='Title'>
-						<Input type='text' placeholder='Title' />
-					</Form.Item>
-				</Col>
-			</Row>
-			<Button type='primary'>Submit</Button>
+		<Form>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Form.Item name=''>
+					<Select style={{ width: 400 }}>
+						<Option>Benefactor / Sponsor Form</Option>
+					</Select>
+				</Form.Item>
+				<Form.Item name=''>
+					<Search
+						placeholder='Search for record'
+						onSearch={value => console.log(value)}
+						style={{ width: 400 }}
+					/>
+				</Form.Item>
+				<h1>Benefactor / Sponsor Form</h1>
+				<Row>
+					<Col>
+						<Form.Item label='Listing notes' {...fieldProps}>
+							<Controller
+								as={TextArea}
+								type='text'
+								control={control}
+								name='listing_notes'
+								placeholder='Listing notes'
+								style={{ width: 600 }}
+							/>
+						</Form.Item>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<Form.Item label='Personal notes' {...fieldProps}>
+							<Controller
+								as={TextArea}
+								type='text'
+								control={control}
+								name='personal_notes'
+								placeholder='Personal notes'
+								style={{ width: 600 }}
+							/>
+						</Form.Item>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<Form.Item label='Title' {...fieldProps}>
+							<Controller control={control} name='title' as={Input} type='text' placeholder='Title' />
+						</Form.Item>
+					</Col>
+				</Row>
+				<Controller name='submit' as={Button} type='primary' htmlType='submit' control={control}>
+					Submit
+				</Controller>
+			</form>
 		</Form>
 	);
 };
