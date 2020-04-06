8570e2dd0ac308ca23bfab5e9f37b7e8ce35cdfe
ebisLab <ebaiden@techie.com>
2020-04-06 16:38:36 -0400

    input validation required message and start of radio button config

--- src/components/forms/Example.js
+++ src/components/forms/Example.js
@@ -10,7 +10,7 @@ const { Option } = Select;
 const defaultValues = { singleErrorInput: "" };
 
 export default function App() {
-  const { errors, handleSubmit, control, register, setValue} = useForm();
+  const { errors, handleSubmit, control, register, setValue } = useForm();
   const [dataInput, setDataInput] = useState(defaultValues);
 
   const onSubmit = data => {
@@ -21,35 +21,35 @@ export default function App() {
     console.log(`selected ${value}`);
   }
 
-  const validError = (value)=>{
+  const validError = (value) => {
     return value.length >= 0 || "error message"
   }
 
   const fieldProps = {
-		colon : false,
+    colon: false,
   };
-  
+
   const emailError = errors.email && 'Enter your email address';
 
 
 
   return (
     <>
-  
-  {console.log('errors', errors)}
+
+      {console.log('errors', errors)}
       <form onSubmit={handleSubmit(onSubmit)}>
 
-      <Form.Item label='Normal Input'>
-        <Controller
-          as={Input}
-          control={control}
-          name="normal_input"
-          rules={{
-            required: true,
-            validate: validError
-          }}
-        />
-        {errors.normal_input && <span>Required</span>}
+        <Form.Item label='Normal Input'>
+          <Controller
+            as={Input}
+            control={control}
+            name="normal_input"
+            rules={{
+              required: true,
+              validate: validError
+            }}
+          />
+          {errors.normal_input && <span>Required</span>}
         </Form.Item>
 
 
@@ -58,20 +58,20 @@ export default function App() {
         // validateStatus="error"
         // help="Should be combination of numbers & alphabets"
         >
-        <Controller
-          as={Input}
-          control={control}
-          name="normal_input_custom"
-          rules={{
-            required: true,
-            validate: validError,
-            validateStatus:"error"
-          }}
-        />
-        {errors.normal_input_custom && <span>Required</span>}
+          <Controller
+            as={Input}
+            control={control}
+            name="normal_input_custom"
+            rules={{
+              required: true,
+              validate: validError,
+              validateStatus: "error"
+            }}
+          />
+          {errors.normal_input_custom && <span>Required</span>}
         </Form.Item>
 
-    {/* <Form.Item name='method-of-add' label='Method of Add'>
+        {/* <Form.Item name='method-of-add' label='Method of Add'>
       <Controller as={Select} control={control} name='method-of-add'>
       <Option value="jack">Jack</Option>
       <Option value="lucy">Lucy</Option>
@@ -80,7 +80,7 @@ export default function App() {
     </Form.Item> */}
 
 
-    {/* <Controller 
+        {/* <Controller 
                 as={Input} 
                 name="email" 
                 control={control} 
@@ -92,9 +92,9 @@ export default function App() {
                 help={emailError}
             /> */}
 
-           
 
-{/* <Controller
+
+        {/* <Controller
         name="gemail"
         control={control}
         rules={{ required: "Please enter your email address" }}
@@ -110,64 +110,64 @@ export default function App() {
       /> */}
 
 
-<Form.Item label='React Select'>
-            <Controller
-              as={ReactSelect}
-              options={[
-                { value:"chocolate", label: "Chocolate" },
-                // { value: "strawberry", label: "Strawberry" },
-                // { value: "vanilla", label: "Vanilla" }
-              ]}
-              name="ReactSelect"
-              isClearable
-              control={control}
-            />
-            </Form.Item>
+        <Form.Item label='React Select'>
+          <Controller
+            as={ReactSelect}
+            options={[
+              { value: "chocolate", label: "Chocolate" },
+              // { value: "strawberry", label: "Strawberry" },
+              // { value: "vanilla", label: "Vanilla" }
+            ]}
+            name="ReactSelect"
+            isClearable
+            control={control}
+          />
+        </Form.Item>
 
 
-            <Form.Item label='React Select With Validation'>
-            <Controller
-              as={ReactSelect}
-              options={[
-                { value: "strawberry", label: "Strawberry" },
-                { value: "vanilla", label: "Vanilla" }
-              ]}
-              rules={{required: true}}
-              name="reac_select_validation"
-              isClearable
-              control={control}
-            />
-              {errors.reac_select_validation && <span>Required</span>}
+        <Form.Item label='React Select With Validation'>
+          <Controller
+            as={ReactSelect}
+            options={[
+              { value: "strawberry", label: "Strawberry" },
+              { value: "vanilla", label: "Vanilla" }
+            ]}
+            rules={{ required: true }}
+            name="reac_select_validation"
+            isClearable
+            control={control}
+          />
+          {errors.reac_select_validation && <span>Required</span>}
 
-            </Form.Item>
+        </Form.Item>
 
 
-            <section>
-            <Form.Item label='Photography allowed'>
+        <section>
+          <Form.Item label='Photography allowed'>
             <Controller
               as={
                 <Radio.Group>
-					<Form.Item label='Yes'>
-						<Controller as={Radio} control={control} name='example' value="1" />
-					</Form.Item>
-					<Form.Item label='No'>
-						<Controller as={Radio} control={control} name='example' value="2" />
-					</Form.Item>
-				</Radio.Group>
+                  <Form.Item label='Yes'>
+                    <Controller as={Radio} control={control} name='example' value="1" />
+                  </Form.Item>
+                  <Form.Item label='No'>
+                    <Controller as={Radio} control={control} name='example' value="2" />
+                  </Form.Item>
+                </Radio.Group>
               }
               name="photography"
               control={control}
             />
-            </Form.Item>
-          </section>
+          </Form.Item>
+        </section>
 
 
-          <section>
+        <section>
           <Form.Item label='Radio Group'>
             <Controller
               as={
                 <Radio.Group aria-label="gender">
-                  
+
                   {/* <Radio value={1}>A</Radio>
         <Radio value={2}>B</Radio>
         <Radio value={3}>C</Radio>
@@ -177,132 +177,192 @@ export default function App() {
               name="radio_group"
               control={control}
             />
-            </Form.Item>
-          </section>
+          </Form.Item>
+        </section>
 
 
-<label>Awesome</label>
-          <Radio.Group ref={register({required: false})} name="awesome">
-        <Radio value={1} label="A">A</Radio>
-        <Radio value={2}>B</Radio>
-        <Radio value={3}>C</Radio>
-        <Radio value={4}>D</Radio>
-      </Radio.Group>
+        <label>Awesome</label>
+        <Radio.Group ref={register({ required: false })} name="awesome">
+          <Radio value={1} label="A">A</Radio>
+          <Radio value={2}>B</Radio>
+          <Radio value={3}>C</Radio>
+          <Radio value={4}>D</Radio>
+        </Radio.Group>
 
-      <section>
-      <Form.Item label='RadioKilla'>
+        <section>
+          <Form.Item label='RadioKilla'>
             <Controller
               as={Radio}
               options={[
-                { value:"chocolate", label: "Chocolate" },
+                { value: "chocolate", label: "Chocolate" },
                 { value: "strawberry", label: "Strawberry" },
                 // { value: "vanilla", label: "Vanilla" }
               ]}
               name="RadioKilla"
               control={control}
             />
-            </Form.Item>
-      </section>
-         
+          </Form.Item>
+        </section>
 
 
-      <section>
-            <Form.Item label='Genderallowed'>
+
+        <section>
+          <Form.Item label='Genderallowed'>
             <Controller
               as={
-     <>
-     {/* <input type="radio" id="male" name="gender2" value="male" ref={register({required: false})}/>
+                <>
+                  {/* <input type="radio" id="male" name="gender2" value="male" ref={register({required: false})}/>
   <label for="male">Male</label>
   <input type="radio" id="female" name="gender2" value="female" ref={register({required: false})}/>
   <label for="female">Female</label>
   <input type="radio" id="other" name="gender2" value="other" ref={register({required: false})}/>
   <label for="other">Other</label> */}
-   <select name="Developer" ref={register({ required: true })}>
-        <option value="Yes">Yes</option>
-        <option value="No">No</option>
-      </select>
-  </>
+                  <select name="Developer" ref={register({ required: true })}>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                  </select>
+                </>
               }
               name="class_intensity2"
               control={control}
             />
-            </Form.Item>
-          </section>
+          </Form.Item>
+        </section>
 
 
 
-          <section>
-            <Form.Item label='Genderallowed 2'>
+        <section>
+          <Form.Item label='Genderallowed 2'>
             <Controller
               as={
-     <>
-     <Radio type="radio" id="male" name="gender1" value="male" ref={register({required: false})}/>
-  <label for="male">Male</label>
-  <Radio type="radio" id="female" name="gender1" value="female" ref={register({required: false})}/>
-  <label for="female">Female</label>
-  <Radio type="radio" id="other" name="gender1" value="other" ref={register({required: false})}/>
-  <label for="other">Other</label>
-  </>
+                <>
+                  <Radio type="radio" id="male" name="gender1" value="male" ref={register({ required: false })} />
+                  <label for="male">Male</label>
+                  <Radio type="radio" id="female" name="gender1" value="female" ref={register({ required: false })} />
+                  <label for="female">Female</label>
+                  <Radio type="radio" id="other" name="gender1" value="other" ref={register({ required: false })} />
+                  <label for="other">Other</label>
+                </>
               }
               name="class_gender2"
               control={control}
             />
-            </Form.Item>
-          </section>
+          </Form.Item>
+        </section>
 
-          <section>
-            <Form.Item label='Genderallowed 3'>
+        <section>
+          <Form.Item label='Genderallowed 3'>
             <Controller
-            type="radio"
+              type="radio"
               as={Radio}
               name="class_gender3"
-        
+
               options={[
-                { value:"nigerian", label: "Nigegerian" },
+                { value: "nigerian", label: "Nigegerian" },
                 { value: "ghana", label: "Ghana" },
                 // { value: "vanilla", label: "Vanilla" }
               ]}
               control={control}
-              setValue={setValue}
             >
 
 
-            {/* <Radio type="radio" id="male" name="gender3" value="male"/> */}
-  <label for="male">Male</label>
-  {/* <Controller as={Radio} control={control} type="radio" id="female" name="class_gender3" value="female">Female</Controller>
+              {/* <Radio type="radio" id="male" name="gender3" value="male"/> */}
+              <label for="male">Male</label>
+              {/* <Controller as={Radio} control={control} type="radio" id="female" name="class_gender3" value="female">Female</Controller>
   <Controller as={Radio} control={control} type="radio" id="female2" name="class_gender" value="female2">Female2</Controller> */}
 
-  {/* <Radio type="radio" id="female" name="gender3" value="female" ref={register({required: false})}/>
+              {/* <Radio type="radio" id="female" name="gender3" value="female" ref={register({required: false})}/>
   <label for="female">Female</label>
   <Radio type="radio" id="other" name="gender3" value="other" ref={register({required: false})}/>
   <label for="other">Other</label> */}
-</Controller>
-            </Form.Item>
-           
-          </section>
+            </Controller>
+          </Form.Item>
 
-          <section>
+        </section>
 
-<Form.Item>
-    <Controller
-    type="radio"
-    as={Radio.Group}
-    rules= {[{
-      required: true,
-      message: 'Please Choose your option!',
-      }]}
-      as={<>
-          <Radio name="chicko" label="true" value={true} ref={register({required: false})}>Taxable</Radio>
-          <Radio name="chicko" label="false" value={false} ref={register({required: false})}>Non Taxable</Radio>
-      </>}
-      control={control}
-      name="chicko"
-    />
+        <section>
 
-</Form.Item>
-          </section>
+          <Form.Item>
+            <Controller
+              type="radio"
+              as={Radio.Group}
+              rules={[{
+                required: true,
+                message: 'Please Choose your option!',
+              }]}
+              // as={<>
+              //     <Controller type="radio" name="chicko" as={Radio} control={control} value="why1" label="selected 1">1</Controller>
+              //     <Controller type="radio" name="chicko" as={Radio} control={control} value="why2" label="Selected 2"/>
 
-{/* <section>
+              //  {/* <input type="radio" id="male" name="gender" value="male"/>
+              //  <label for="male">Male</label>
+              //  <input type="radio" id="female" name="gender" value="female"/>
+              //  <label for="male">Female</label> */}
+              //     {/* <Radio name="chicko" label="true" value={true} ref={register({required: false})}>Taxable</Radio>
+              //     <Radio name="chicko" label="false" value={false} ref={register({required: false})}>Non Taxable</Radio> */}
+              // </>}
+              control={control}
+              name="chicko"
+            >
+
+              <Controller type="radio" name="chicko" as={Radio} control={control} value="why1" label="selected 1">1</Controller>
+              <Controller type="radio" name="chicko" as={Radio} control={control} value="why2" label="Selected 2" />
+
+
+            </Controller>
+
+
+          </Form.Item>
+        </section>
+
+        <section>
+DAY 3
+
+        <Form.Item label='DAY5'>
+            <Controller
+              as={
+                <Radio.Group aria-label="gender">
+
+                  {/* <Radio value={1}>A</Radio>
+        <Radio value={2}>B</Radio>
+        <Radio value={3}>C</Radio>
+        <Radio value={4}>D</Radio> */}
+                </Radio.Group>
+              }
+              name="radio_group"
+              control={control}
+            >
+              <Radio aria-label="optionB">Option B</Radio>
+              <Radio aria-label="optionC">Option C</Radio>
+              <Radio aria-label="optionA">Option A</Radio>
+              {/* <Controller
+              as={
+                <Radio aria-label="optionA">Option A</Radio>
+              }
+              name="optionA"
+              control={control}
+            />
+            <Controller
+              as={
+                <Radio aria-label="optionB">Option B</Radio>
+              }
+              name="optionB"
+              control={control}
+            />
+            <Controller
+              as={
+                <Radio aria-label="optionC">Option C</Radio>
+              }
+              name="optionC"
+              control={control}
+            />
+               */}
+               </Controller>
+          </Form.Item>
+        </section>
+
+
+        {/* <section>
             <label>React Select</label>
             <Controller
               as={ReactSelect}
@@ -320,20 +380,20 @@ export default function App() {
 
         {/* <button type="submit">Submit</button> */}
 
-      {/*Fixed button*/}
+        {/*Fixed button*/}
         <Button block type="primary" htmlType="submit">
-        Submit
+          Submit
       </Button>
 
-        
+
       </form>
 
-{console.log('dataInput insdie', dataInput)}
+      {console.log('dataInput insdie', dataInput)}
 
-      
+
 
       <pre style={{ marginTop: 54 }}>
-      {JSON.stringify(dataInput, null, 2)}
+        {JSON.stringify(dataInput, null, 2)}
       </pre>
     </>
   );
