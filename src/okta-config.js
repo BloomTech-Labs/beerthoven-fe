7490541bd62933584a26833ac096b59b0d315fac
Nolan Picini <nolanpic@gmail.com>
2020-04-19 23:34:22 -0700

    Okta signin working

--- src/okta-config.js
+++ src/okta-config.js
@@ -1,15 +1,18 @@
-const OKTA_DOMAIN = process.env.DOMAIN;
-const CLIENT_ID = process.env.CLIENT_ID;
-const CALLBACK_PATH = '/implicit/callback';
+const OKTA_DOMAIN =
+  process.env.REACT_APP_OKTA_DOMAIN || process.env.OKTA_DOMAIN;
+const CLIENT_ID =
+  process.env.REACT_APP_OKTA_CLIENTID || process.env.OKTA_CLIENTID;
+const CALLBACK_PATH = "/implicit/callback";
 
 const ISSUER = `https://${OKTA_DOMAIN}/oauth2/default`;
 const HOST = window.location.host;
 const REDIRECT_URI = `http://${HOST}${CALLBACK_PATH}`;
-const SCOPES = 'openid profile email';
+const SCOPES = "openid profile email";
 
-export default = {
+export default {
   issuer: ISSUER,
   clientId: CLIENT_ID,
   redirectUri: REDIRECT_URI,
   scope: SCOPES.split(/\s+/),
-});
+  callbackPath: CALLBACK_PATH,
+};
