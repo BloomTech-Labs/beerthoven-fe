209c5f035d9b7ff99df7c0355667c85cd13c4495
ebisLab <ebaiden@techie.com>
2020-04-22 11:50:36 -0400

    fixed className and proptype

--- src/components/pages/Chart.js
+++ src/components/pages/Chart.js
@@ -5,7 +5,7 @@ import { ALL_EVENTS } from '../graphql/queries';
 import '../../index.css'
 const Chart = () => {
 
-  const [chartData, setChartData] = useState([])
+  const [chartData, setChartData] = useState({})
 
 const { data } = useQuery(ALL_EVENTS);
 
@@ -19,7 +19,7 @@ const { data } = useQuery(ALL_EVENTS);
         setChartData({
             labels: stateNames,
             datasets: [{
-              label: '# of Votes',
+              label: '# of events in state',
               data: statesNum,
               backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
@@ -101,35 +101,18 @@ const { data } = useQuery(ALL_EVENTS);
 
   return (<div>
     
-{data && data.events.length &&  
-
-(
-    <>
-    <p>Line Graph</p>
-<ChartSection chartData={chartData} list={data.events}/>
-</>
-)
-  }
+{data && data.events.length &&  <ChartSection chartData={chartData} list={data.events}/>}
   </div>)
-
-
-
-
 }
 
-const ChartSection = ({list, chartData})=>{
-    
-    const data = list.map(item => item.state);
-    console.log(data, '<**data')
-
-
+const ChartSection = ({chartData})=>{
+console.log(chartData.datasets, 'chartdata*****')
     return(
         <div className="container">
         
-        <div class="chart-container">
+        <div className="chart-container">
         Pie Graph
         <Pie
-        // width={700}
            data={chartData}
            options={{
              title: {
@@ -141,13 +124,12 @@ const ChartSection = ({list, chartData})=>{
                display: true,
                position: 'left'
              },
-             maintainAspectRatio: false
            }}
            
          />
          </div>
 
-<div class="chart-container">
+<div className="chart-container">
 Doughnut Graph
              <Doughnut
       data={chartData}
@@ -165,12 +147,12 @@ Doughnut Graph
     />
     </div>
 
-    <div class="chart-container">
+    <div className="chart-container">
 Bar char 
          <Bar data={chartData} /> 
          </div>
 
-         <div class="chart-container">
+         <div className="chart-container">
 
     Line
         <Line
