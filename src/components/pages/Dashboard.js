17000e3c0eabc3242ad76b6c7ffc3ca561e758e2
Nolan Picini <nolanpic@gmail.com>
2020-04-22 19:07:35 -0700

    Removed unneeded menu from dashboard

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,30 +1,12 @@
-import React from 'react';
-import { Menu } from 'antd';
-import { Link } from 'react-router-dom';
-import Chart from './Chart';
+import React from "react";
+import Chart from "./Chart";
 
 const Dashboard = () => (
-	<div>
-		<h1>Dashboard</h1>
-		<p style={{ fontSize: '90px' }}>Welcome!</p>
-		<Chart/>
-		<Menu>
-			<Menu.ItemGroup key='h1' title='Manage'>
-				<Menu.Item key='1'>
-					<Link to='/people'>People</Link>
-				</Menu.Item>
-				<Menu.Item key='2'>
-					<Link to='/event'>Events</Link>
-				</Menu.Item>
-				<Menu.Item key='3'>
-					<Link to='/venue'>Venues</Link>
-				</Menu.Item>
-				<Menu.Item key='4'>
-					<Link to='/user'>Users</Link>
-				</Menu.Item>
-			</Menu.ItemGroup>
-		</Menu>
-	</div>
+  <div>
+    <h1>Dashboard</h1>
+    <p style={{ fontSize: "90px" }}>Welcome!</p>
+    <Chart />
+  </div>
 );
 
 export default Dashboard;
