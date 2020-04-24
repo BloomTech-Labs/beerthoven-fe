85defc76cfc14e4c1460941b2193818c67a33ac6
ebisLab <ebaiden@techie.com>
2020-04-24 17:27:46 -0400

    Chart tests

---
+++ src/components/pages/Chart/tests/ChartLine.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ChartLine from '../ChartLine';
+import { act } from 'react-dom/test-utils'
+
+it('renders without crashing', () => {
+    // act(() => {
+    //     const div = document.createElement('div');
+    //     ReactDOM.render(<ChartLine />, div);
+    // })
+})
